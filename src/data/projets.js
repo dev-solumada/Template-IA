d255a00407fb448d3ac3cb72a843357dafe08f33
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-31 13:20:28 +0300

    ADD projets data

---
+++ src/data/projets.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        "selected" : false,
+        "client" : "Mathieu",
+        "name" :"Personnalisation",
+        "created_at" : "2018-12-10T13:49:51.141Z",
+        "updated_at" : "2018-12-10T13:49:51.141Z",
+        "client_id" : "123456789",
+        "document_id" : "123456789"
+    },
+    {
+        "selected" : false,
+        "client" : "Clara",
+        "name" :"Impression",
+        "created_at" : "2018-12-10T13:49:51.141Z",
+        "updated_at" : "2018-12-10T13:49:51.141Z",
+        "client_id" : "123456789",
+        "document_id" : "123456789"
+    },
+    {
+        "selected" : false,
+        "client" : "Clara",
+        "name" :"Impression",
+        "created_at" : "2018-12-10T13:49:51.141Z",
+        "updated_at" : "2018-12-10T13:49:51.141Z",
+        "client_id" : "123456789",
+        "document_id" : "123456789"
+    }
+]
