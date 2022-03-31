98c9b7f5539223588c686a7569461179b73449e2
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-31 14:23:43 +0300

    UPDATE project, ADD tables_c

---
+++ src/components/Tables_c.js
@@ -0,0 +1,84 @@
+
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleUp, faArrowDown, faArrowUp, faEdit, faEllipsisH, faExternalLinkAlt, faEye, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row, Nav, Card, Image, Button, Table, Dropdown, ProgressBar, Pagination, ButtonGroup } from '@themesberg/react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import { Routes } from "../routes";
+import { pageVisits, pageTraffic, pageRanking } from "../data/tables";
+import transactions from "../data/transactions";
+import commands from "../data/commands";
+
+const ValueChange = ({ value, suffix }) => {
+  const valueIcon = value < 0 ? faAngleDown : faAngleUp;
+  const valueTxtColor = value < 0 ? "text-danger" : "text-success";
+
+  return (
+    value ? <span className={valueTxtColor}>
+      <FontAwesomeIcon icon={valueIcon} />
+      <span className="fw-bold ms-1">
+        {Math.abs(value)}{suffix}
+      </span>
+    </span> : "--"
+  );
+};
+
+
+export const ProjectTable = () => {
+  const TableRow = (props) => {
+    const { country, countryImage, overallRank, overallRankChange, travelRank, travelRankChange, widgetsRank, widgetsRankChange } = props;
+
+    return (
+      <tr>
+        <td className="border-0">
+          <Card.Link href="#" className="d-flex align-items-center">
+            <Image src={countryImage} className="image-small rounded-circle me-2" />
+            <div><span className="h6">{country}</span></div>
+          </Card.Link>
+        </td>
+        <td className="fw-bold border-0">
+          {overallRank ? overallRank : "-"}
+        </td>
+        <td className="border-0">
+          <ValueChange value={overallRankChange} />
+        </td>
+        <td className="fw-bold border-0">
+          {travelRank ? travelRank : "-"}
+        </td>
+        <td className="border-0">
+          <ValueChange value={travelRankChange} />
+        </td>
+        <td className="fw-bold border-0">
+          {widgetsRank ? widgetsRank : "-"}
+        </td>
+        <td className="border-0">
+          <ValueChange value={widgetsRankChange} />
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <Card border="light" className="shadow-sm">
+      <Card.Body className="pb-0">
+        <Table responsive className="table-centered table-nowrap rounded mb-0">
+          <thead className="thead-light">
+            <tr>
+              <th className="border-0">Country</th>
+              <th className="border-0">All</th>
+              <th className="border-0">All Change</th>
+              <th className="border-0">Travel & Local</th>
+              <th className="border-0">Travel & Local Change</th>
+              <th className="border-0">Widgets</th>
+              <th className="border-0">Widgets Change</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pageRanking.map(r => <TableRow key={`ranking-${r.id}`} {...r} />)}
+          </tbody>
+        </Table>
+      </Card.Body>
+    </Card>
+  );
+};
--- src/pages/Projets/Projets.js
+++ src/pages/Projets/Projets.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Breadcrumb } from '@themesberg/react-bootstrap';
+
+import { ProjectTable } from "../../components/Tables_c";
 
 const Projet = () => {
-    return <div>Projet</div>;
+  return (
+    <>
+      <div className="d-xl-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+        <div className="d-block mb-4 mb-xl-0">
+          <Breadcrumb className="d-none d-md-inline-block" listProps={{ className: "breadcrumb-dark breadcrumb-transparent" }}>
+            <Breadcrumb.Item><FontAwesomeIcon icon={faHome} /></Breadcrumb.Item>
+            <Breadcrumb.Item>Tables</Breadcrumb.Item>
+            <Breadcrumb.Item active>Bootstrap tables</Breadcrumb.Item>
+          </Breadcrumb>
+          <h4>Project List</h4>
+          <p className="mb-0">
+            Liste des projets ici avec la date de création et de modification
+          </p>
+        </div>
+      </div>
+      
+      <ProjectTable />
+    </>
+  );
 };
 
+
 export default Projet;
+
+// component : page/components/Tables/bootstrapTable
