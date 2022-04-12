1a46bb4e53ddc7b2625023524a51558045ea2a24
yunisuthn <yunis.thn@gmail.com>
2022-04-12 15:21:06 +0300

    ADD ViewPDF: pas encore marché

--- src/pages/Documents/viewPDF.js
+++ src/pages/Documents/viewPDF.js
@@ -1,4 +1,26 @@
+// import React, { Component } from 'react';
+// import logger from 'logging-library';
+// import FileViewer from 'react-file-viewer';
+// import { CustomErrorComponent } from 'custom-error';
 
+// const file = '../../'
+// const type = 'pdf'
+
+// class MyDocComponent extends Component {
+//     render() {
+//         return (
+//             <FileViewer
+//                 fileType={type}
+//                 filePath={file}
+//                 errorComponent={CustomErrorComponent}
+//                 onError={this.onError} />
+//         );
+//     }
+
+//     onError(e) {
+//         logger.logError(e, 'error in file-viewer');
+//     }
+// }
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf';
 
@@ -23,3 +45,32 @@ function ViewPDF() {
     );
 }
 export default ViewPDF;
+
+// import { useEffect, useRef } from 'react';
+// import WebViewer from '@pdftron/webviewer'
+
+// const ViewPDF = () => {
+//     const viewer = useRef(null);
+
+//     useEffect(() => {
+//         WebViewer(
+//             {
+//                 path: '/webviewer/lib',
+//                 initialDoc: '/files/pdftron_about.pdf',
+//             },
+//             viewer.current,
+//         ).then((instance) => {
+//             const { documentViewer } = instance.Core;
+//             // you can now call WebViewer APIs here...
+//         });
+//     }, []);
+
+//     return (
+//         <div className="MyComponent">
+//             <div className="header">React sample</div>
+//             <div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
+//         </div>
+//     );
+// };
+
+// export default ViewPDF;
